fa59e7d06e2ca7514b3afc1e3fa57fded4a80ad8
kittenmasque <volaju@gmail.com>
2023-02-11 21:36:22 -0800

    created login form for the header

--- header.php
+++ header.php
@@ -9,5 +9,22 @@
 <body>
 
 <header>
-
+<nav>
+    <a href="#">
+        <img src="img/logo.png" alt="logo">
+    </a>
+    <ul>
+        <li><a href="index.php">Home</a></li>
+        <li><a href="#">Portfolio</a></li>
+        <li><a href="#">About</a></li>
+        <li><a href="#">Contact</a></li>
+    </ul>
+<div>
+<form action="includes/login.inc.php" method="post">
+    <input type="text" name="mailuid" value="Username/E-mail..">
+    <input type="password" name="pwd" value="Password">
+<button type="Submit" name="login-submit">Login</button>
+</form>
+</div>
+</nav>
 </header>
